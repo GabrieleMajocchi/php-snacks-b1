97e8373a50be4caa5846a43f89f0a56c0fa6adad
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-03 13:02:24 +0200

    improved login response page

--- Snack-2/response.php
+++ Snack-2/response.php
@@ -1,11 +1,49 @@
-<?php
-        $name = $_POST["name"];
-        $mail = $_POST["mail"];
-        $age = $_POST["age"];
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Risultato Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+            $name = $_POST["name"];
+            $mail = $_POST["mail"];
+            $age = $_POST["age"];
 
-        if (strlen($name) > 3 && strpos($mail, "@") !== false && strpos($mail, ".") !== false && is_numeric($age)) {
-            echo "Accesso riuscito";
-        } else {
-            echo "Accesso negato";
-        }
-    ?>
+            if (strlen($name) > 3 && strpos($mail, "@") !== false && strpos($mail, ".") !== false && is_numeric($age)) {
+                echo "Accesso riuscito";
+            } else {
+                echo "Accesso negato";
+            }
+        ?>
+    </div>
+</body>
+</html>
+
+<style>
+body {
+    background-color: #f2f2f2;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    margin: 100px auto;
+    max-width: 400px;
+    padding: 20px;
+    text-align: center;
+}
+
+h2 {
+    color: #333;
+}
+
+p {
+    color: #555;
+}
+</style>
