06061507c7427607bc37ab5184df7a5a433924c1
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-04 12:51:40 +0200

    added an extra check in case the range start or end from value that are not in the array

--- Snack-4/index.php
+++ Snack-4/index.php
@@ -1,11 +1,17 @@
 <?php 
 
 $array = ['a', 'b', 'c', 'd', 'e'];
-$a = 1;
-$b = 3;
+$a = -5;
+$b = 7;
 
 function getArrayValues($array, $startValue, $endValue) {
     $selecterArray = [];
+    if($startValue < 0){
+        $startValue = 0;
+    }
+    if($endValue >= count($array)){
+        $endValue = count($array) - 1;
+    }
     for ($i = $startValue; $i <= $endValue; $i++) {
         $selecterArray[] = $array[$i];
     }
